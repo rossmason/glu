0bf225ea525e092a064626ea20ad7bb27e007c80
jbrendel <juergen@brendel.com>
2010-05-11 10:50:48 +1200

    Added an overwrite for dumps(), which uses a custom JSON encoder to deal with datetime and date types.

--- src/python/glujson.py
+++ src/python/glujson.py
@@ -1,4 +1,6 @@
 
+import datetime
+
 from glu.platform_specifics import *
 
 if PLATFORM == PLATFORM_PYTHON:
@@ -7,3 +9,20 @@ elif PLATFORM == PLATFORM_GAE:
     from django.utils.simplejson import *
 else:
     from simplejson import *
+
+class GluJsonEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime)  or isinstance(obj, datetime.date):
+            return str(obj)
+        return JSONEncoder.default(self, obj)
+
+def dumps(obj):
+    """
+    Encode an object to string, using our own JSON Encoder.
+
+    This method hides the default 'dumps' method that comes with
+    the JSON module.
+
+    """
+    return GluJsonEncoder().encode(obj)
+
