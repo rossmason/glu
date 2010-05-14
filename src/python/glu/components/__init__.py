1370596d607ed783081dc58cb41b040de603d37c
jbrendel <juergen@brendel.com>
2010-05-14 12:59:28 +1200

    Commented out the Salesforce and Marakana components.

--- src/python/glu/components/__init__.py
+++ src/python/glu/components/__init__.py
@@ -16,14 +16,14 @@ from glu.components.gsearch_component      import GsearchComponent
 from glu.components.combiner_component     import CombinerComponent
 from glu.components.gpswalker_component    import GpsWalkerComponent
 from glu.components.storage_component      import StorageComponent
-from glu.components.salesforce_component   import SalesforceComponent
-from glu.components.marakana_component     import MarakanaComponent
+#from glu.components.salesforce_component   import SalesforceComponent
+#from glu.components.marakana_component     import MarakanaComponent
 
 #
 # The known components
 #
-_KNOWN_COMPONENTS = [ TwitterComponent, GsearchComponent, CombinerComponent, GpsWalkerComponent, StorageComponent,
-                      SalesforceComponent, MarakanaComponent ]
+_KNOWN_COMPONENTS = [ TwitterComponent, GsearchComponent, CombinerComponent, GpsWalkerComponent, StorageComponent ]
+# SalesforceComponent, MarakanaComponent ]
 
 # -------------------------------------------------------------------------------------
 # Don't edit down here...
